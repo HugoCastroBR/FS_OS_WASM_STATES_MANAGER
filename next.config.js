6030c5644b57c73ec57773b1792b8c48c6214a58
HugoCastroBr <hugoecastro2008@hotmail.com>
2023-11-16 12:57:04 -0300

    geting packages needed

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack(config) {
+    config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm'
+    config.experiments = { asyncWebAssembly: true }
+    return config
+  },
+}
 
 module.exports = nextConfig
